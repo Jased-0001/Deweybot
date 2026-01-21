832c77a4686e3289011f56325f262e59661a66d0
Jase Williams <jased@jased.xyz>
2026-01-21 18:50:59 -0500

    i think i cleaned stuff up (changed the token file yaml and added some options)

--- Permissions.py
+++ Permissions.py
@@ -1,16 +1,15 @@
 from discord.ext import commands
+import Bot
 
-override_users = [
-    322495136108118016, # JASE
-    940684920966250567,  # jinxxxddd_
-]
+override_users = Bot.DeweyConfig["permission-override"]
 
+print(override_users)
 #[y.id for y in ctx.user.roles]
 
 def banned(ctx):
     user_roles = [y.id for y in ctx.user.roles] # pyright: ignore[reportAttributeAccessIssue]
     for i in user_roles:
-        if i == 1463430130167709804:
+        if i == Bot.DeweyConfig["banned-role"]:
             return True
     return False
 
