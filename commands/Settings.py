1ceed11905c3ee4ff8fd7b3b2b6ece98b911f9e6
Jase Williams <jased@jased.xyz>
2026-05-03 00:15:56 -0400

    la la la 2

--- commands/Settings.py
+++ commands/Settings.py
@@ -10,14 +10,14 @@ import other.Settings as Settings
 gfad_group = discord.app_commands.Group(name="settings", description="Settings")
 
 @gfad_group.command(name="z-debug-get", description="cause uptown funk gonna give it to ya")
-async def get(ctx : discord.Interaction, database_ident:str,name:str):
+async def get(ctx : discord.Interaction,name:str):
     if Permissions.is_override(ctx):
         a = Settings.Settings().get_setting(uid=ctx.user.id, name=name)
         await ctx.response.send_message(content=a, ephemeral=True)
 
 
 @gfad_group.command(name="z-debug-set", description="cause uptown funk gonna give it to ya")
-async def set(ctx : discord.Interaction, database_ident:str,name:str,value:bool):
+async def set(ctx : discord.Interaction,name:str,value:bool):
     if Permissions.is_override(ctx):
         Settings.Settings().set_setting(uid=ctx.user.id, name=name, value=value)
         await ctx.response.send_message(content="ok", ephemeral=True)
